9d26a941986e14bfef2da2d259cc5c19ec48e388
tfang7 <dkfang7@gmail.com>
2016-04-25 17:48:55 -0400

    fixed some bugs

--- Assets/Scripts/FlockMember.cs
+++ Assets/Scripts/FlockMember.cs
@@ -1,32 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent (typeof(Rigidbody))]
 public class FlockMember : MonoBehaviour {
     public float lifetime = 1.0f;
     public bool inFlock = false;
     float startTime;
     float lifeTimer;
+    GameObject Player;
 	// Use this for initialization
 	void Start () {
         startTime = Time.time;
         lifeTimer = startTime + lifetime;
-	}
+        Player = GameObject.FindGameObjectWithTag("Player");
+        
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
         if (lifeTimer <= Time.time)
         {
-            //lifeTimer = Time.time + lifetime;
+            lifeTimer = Time.time + lifetime;
             Destroy(gameObject);
+            //Destroy.gameObject);
+
         }
-	    
-	}
+
+    }
     void OnTriggerEnter(Collider c)
     {
         if (c.gameObject.tag == "Player")
-        {
+        {            
             if (gameObject != null)
             {
                 if (inFlock == false)
